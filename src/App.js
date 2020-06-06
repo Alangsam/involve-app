d50cf83cfbbb9e64c43d82722a361ae83bf743eb
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-06-05 20:30:24 -0700

    basic layout landing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style/master.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,15 +1,119 @@
 import React from "react";
 
 export default function Landing() {
-  return (
-    <div className="backGround">
-      <div className="container">
-        <div className="row">
-          <div>
-            <div></div>
-          </div>
+    return (
+        <div className="background-image">
+            <div className="container">
+                <div className="row all-v">
+                    <div className="col-5 mt-6 ">
+                        <div
+                            className="d-flex align-items-start flex-column"
+                            style={{ height: "95vh" }}
+                        >
+                            <div className="card mb-auto">
+                                <h2 className="text-center">Involve</h2>
+                                <p>
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah
+                                </p>
+                            </div>
+                            <div className="my-auto">
+                                <button className="btn btn-primary">
+                                    Create an account, or enter your info
+                                    manually when you find the right cause
+                                </button>
+                            </div>
+                            <div className="mb-7 mt-9 text-white">
+                                <h1>IN ° VOLVE</h1>
+                                <h3>
+                                    cause (a person or group) to experience or
+                                    participate in an activity or situation.
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-6 offset-1">
+                        <div>
+                            <div className="card ">
+                                <h2 className="text-center">Involve</h2>
+                                <p>
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah
+                                </p>
+                            </div>
+                            <div className="card my-6">
+                                <h2 className="text-center">Involve</h2>
+                                <p>
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah
+                                </p>
+                            </div>
+                            <div className="card my-6">
+                                <h2 className="text-center">Involve</h2>
+                                <p>
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah
+                                </p>
+                            </div>
+                            <div className="card my-6">
+                                <h2 className="text-center">Involve</h2>
+                                <p>
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah blah blah blah blah
+                                    blah blah blah blah blah
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
